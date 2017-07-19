7300c5f8c1ebe461661b74f730bbd9c01fac6229
anonymous <xgqfrms@gmail.com>
2017-07-20 00:51:13 +0800

    Update configureStore.js

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,7 +7,6 @@ import rootReducer from '../reducers';
 const configureStoreProd = (initialState) => {
     const middlewares = [
         // Add other middleware on this line...
-
         // thunk middleware can also accept an extra argument to be passed to each thunk action
         // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
         thunk,
@@ -19,8 +18,7 @@ const configureStoreProd = (initialState) => {
             applyMiddleware(...middlewares)
         )
     );
-}
-
+};
 const configureStoreDev = (initialState) => {
     const middlewares = [
         // Add other middleware on this line...
@@ -52,9 +50,8 @@ const configureStoreDev = (initialState) => {
         );
     }
     return store;
-}
+};
 const configureStore = process.env.NODE_ENV === 'production' ? configureStoreProd : configureStoreDev;
-
 export {configureStore};
 export default configureStore;
 
